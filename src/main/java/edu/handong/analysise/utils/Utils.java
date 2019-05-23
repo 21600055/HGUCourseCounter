23411c61f1bf8ba6a9b4c3b5951940a3a57abbe7
user <user@DESKTOP-BGIC6EH>
2019-05-24 02:17:44 +0900

    commit

--- src/main/java/edu/handong/analysise/utils/Utils.java
+++ src/main/java/edu/handong/analysise/utils/Utils.java
@@ -7,7 +7,40 @@ public class Utils {
 	
 	public static ArrayList<String> getLines(String file,boolean removeHeader)
 	{
-		return ;
+		ArrayList<String> aline =new ArrayList<String>();
+		int i;
+		try {
+			
+			File csv= new File(file);
+			BufferedReader br = new BufferedReader(new FileReader(csv));
+            String line = "";
+            while((line=br.readLine())!=null)
+            {
+            	if(removeHeader)
+            	{
+            		removeHeader=false;
+            		continue;
+            	}
+            	
+            	aline.add(line);
+            }
+            i=0;
+            
+            while(i<aline.size())
+            {
+            	aline.get(i);
+            	i++;
+            }
+            br.close();
+            
+            
+		} catch(FileNotFoundException e) {
+			System.out.println("The file path does not exist. Please check your CLI argument!");
+		} catch(IOException e) {
+			
+		}
+		
+		return aline;
 	}
 	
 	public static void writeAFile(ArrayList<String>lines,String targetFileName)
